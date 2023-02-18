c7b4bf502a71ac15f89db0efb7361cf786cde96d
kdot227 <damndc227@gmail.com>
2023-02-18 16:58:31 -0600

    ADD LEVEL 4 WWWWWWWW

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ options = """
 [1] Level 1 (Recommended to use AFTER 2)
 [2] Level 2
 [3] Level 3
+[4] Level 4 (NOTE: Don't end your files with exit when its done with this command or it might break. (same with pause))
 [clean] cleans the code to try and fix any common errors
 [all] does 1, 2, 3 and clean
 [fud] makes it undetectable by everything on virustotal
@@ -71,6 +72,7 @@ class Main:
                 "1": self.level1,
                 "2": self.level2,
                 "3": self.level3,
+                "4": self.level4,
                 "clean": self.clean,
                 "all": self.all,
                 "fud": self.fud,
@@ -521,6 +523,79 @@ goto :eof
                 Main()
 
         return main_list
+    
+    def level4(self):
+        #Level 4 as promised. (I lowkey skidded it from someone else but i cant remember who :skull:) W mans tho ong
+        #Also I can remake this instead of using JScript, etc
+        batch_code = """@if (@CodeSection == @Batch) @then
+
+
+@echo off
+setlocal DisableDelayedExpansion
+
+if "%~1" equ "" echo Usage: Obfuscate filename.bat & goto :EOF
+if not exist "%~1" echo File not found: "%~1" & goto :EOF
+
+set "at=@"
+set "pass=%random%"
+(
+   echo %at%if (@Pass == @X%pass%^) @begin
+   echo    @echo off
+   echo    CScript //nologo //E:JScript.Encode "%%~F0" ^> %pass%.bat
+   echo    call %pass%
+   echo    del /f /q %pass%.bat
+   echo    exit /B
+   echo %at%end 
+   echo //**Start Encode**
+   echo var a = new Array(^);
+
+   set "i=0"
+   for /F "usebackq delims=" %%a in ("%~1") do (
+      set /A i+=1
+      set "line=%%a"
+      setlocal EnableDelayedExpansion
+      echo a[!i!] = '!line:'=\x27!';
+      endlocal
+   )
+
+   setlocal EnableDelayedExpansion
+   echo for ( var i=1; i^<=!i!; ++i ^) WScript.Stdout.WriteLine(a[i]^);
+) > "%~N1.tmp"
+
+CScript //nologo //E:JScript "%~F0" "%~N1.tmp"
+::rename "%~N1.tmp" "%~N1.bat"
+::del "%~N1.tmp"
+rename "%~N1.tmp" "%~N1.encoded.bat"
+goto :EOF
+
+
+@end
+
+//Made by some guy on stack overflow (I can't find the post anymore + I delete the old comments I had :sob:)
+//I had to edit it a lil bit to make it work again. (It didn't work at all for me before)
+
+var fileToEncode = WScript.Arguments(0);
+
+var oFSO = WScript.CreateObject("Scripting.FileSystemObject");
+var oFile = oFSO.GetFile(fileToEncode);
+var oStream = oFile.OpenAsTextStream(1);
+var sSourceFile = oStream.ReadAll();
+oStream.Close();
+
+var oEncoder = WScript.CreateObject("Scripting.Encoder");
+var sDest = oEncoder.EncodeScriptFile(".js", sSourceFile, 0, "")
+
+var edited_name = fileToEncode.replace(".bat", ".encoded.bat");
+
+var oStream = oFSO.OpenTextFile(fileToEncode, 2, true);
+oStream.Write(sDest);
+oStream.Close();"""
+        with open("level4.bat", "w") as f:
+            f.writelines(batch_code)
+        os.system("level4.bat " + self.file)
+        file_name = self.file.replace(".bat", ".encoded.bat")
+        os.rename(f"{file_name}", f"{self.file}.level4.bat")
+        os.remove("level4.bat")
 
 if __name__ == "__main__":
     AutoUpdate()
