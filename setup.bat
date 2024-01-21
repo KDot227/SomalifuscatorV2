df713d62f690f111b95a92651988e4b877285de6
KDot227 <damndc227@gmail.com>
2024-01-20 19:08:07 -0600

    fix for kids

--- setup.bat
+++ setup.bat
@@ -10,26 +10,8 @@ set "python_path=%localappdata%\Programs\Python"
 echo %python_path%
 
 if not exist "%python_path%" (
-    echo "would you like to install python? (y/n)"
-    set /p "install_python="
-    if "%install_python%" == "y" (
-        :: lowkey stole this from addix but I made my own and cant find it
-        for /f "tokens=1,2 delims= " %%a in ('powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/ -UseBasicParsing | Select-String -Pattern '3.11.[0-9]{1,2}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Sort-Object -Descending -Unique | Select-Object -First 1"') do (
-            set "PYTHON_VERSION=%%a%%b"
-        )
-        set "PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe"
-        set "PYTHON_EXE=python-installer.exe"
-
-        curl -L -o %PYTHON_EXE% %PYTHON_URL%
-
-        start /wait %PYTHON_EXE% /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
-
-        del %PYTHON_EXE%
-    ) else (
-        echo please install python and add it to path
-        pause
-        exit /b 1
-    )
+    echo "Python may not be installed. fr fr."
+    pause
 )
 
 python --version
